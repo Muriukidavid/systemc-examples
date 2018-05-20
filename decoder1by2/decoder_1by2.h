83f6e8f44b502762da9e31159e72f946a73cc1fa
WBruss <kihoropaul@gmail.com>
2018-05-20 08:20:36 +0300

    The file driver.h was modified to add the second output (d_b). > decoder_1by2.h was modified to include the new input and to give the decoded output. > monitor.c was modified to give the output of the two inputs and four outputs. The output was format was changed to give a proper output. > decoder_1by2.cc was modified to give the new pin connections and to add the pins to the tracefiles. > I was unable to fix the output of the pins on the terminal. I was unable to ine a resource that would help me edit the tracefile or a resource to help me to sample the signals between edges not at the edges. This is the reson there are duplicate results for each time stamp.

--- decoder1by2/decoder_1by2.h
+++ decoder1by2/decoder_1by2.h
@@ -11,12 +11,13 @@
 
 SC_MODULE(decoder){
 //input and output ports
-sc_in<bool> a;
-sc_out<bool> b,c;
+sc_in<bool> a,b;
+sc_out<bool> c,d,e,f;
 //constructor: where the processes are bound to simulation kernel
 SC_CTOR(decoder){
 	SC_METHOD(decode);
 	sensitive<<a;
+	sensitive<<b;
 	//dont_initialize();
 }
 
@@ -25,12 +26,11 @@ SC_CTOR(decoder){
 }
 
 void decode(void){
-	b=(a==0)?1:0;
-	c=(a==1)?1:0;
+	c=(a==0 && b==0)?1:0;
+	d=(a==1 && b==0)?1:0;
+	e=(a==0 && b==1)?1:0;
+	f=(a==1 && b==1)?1:0;
 }
 };
-
-
-
 
 #endif /* DECODER_2BY4_H_ */
