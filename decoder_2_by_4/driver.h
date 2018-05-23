d385626151d2259630d7326cb614a9895adba6ce
idachi rufusy <ruffiam@yahoo.com>
2018-05-24 00:02:22 +0300

    Add files via upload
    The abstract implementation of a 2 by 4 decoder. The driving signals have been increased to 2 i.e A0 and A1 while the output signals have been increased  to 4 i.e D0, D1, D2 and D3.
    The names of the files have also been changed.

--- decoder_2_by_4/driver.h
+++ decoder_2_by_4/driver.h
@@ -1,1 +1,27 @@
+#ifndef DRIVER_H_
+#define DRIVER_H_
 
+#include<systemc>
+
+SC_MODULE(driver){
+	sc_out<bool>	d_A1;
+	sc_out<bool> 	d_A0;
+
+	SC_CTOR(driver){
+		SC_THREAD(drive);
+	}
+
+	void drive(void){
+		while(1){
+			d_A1 = 0; d_A0 = 0;
+			wait(5,SC_NS);
+			d_A1 = 1; d_A0 = 0;
+			wait(5,SC_NS);
+			d_A1 = 0; d_A0 = 1;
+			wait(5,SC_NS);
+			d_A1 = 1; d_A0 = 1;
+			wait(5,SC_NS);
+		}
+	}
+};
+#endif /* DRIVER_H_ */
