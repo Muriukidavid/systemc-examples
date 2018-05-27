2b4aa618cd2c298862143f172e0ac233294ad024
idachi rufusy <ruffiam@yahoo.com>
2018-05-27 20:36:44 +0300

    Add files via upload

---
+++ 2-to-4-decoder-submodules-2/inverter_operation.h
@@ -0,0 +1,27 @@
+
+#ifndef INVERTER_OPERATION_H_
+#define INVERTER_OPERATION_H_
+#include<systemc.h>
+
+SC_MODULE(inverter){
+	//input and output ports
+	sc_in<bool> x1, k, m;
+	sc_out<bool> out1, out2;
+	//constructor: where the processes are bound to simulation kernel
+	SC_CTOR(inverter){
+		SC_METHOD(invert);
+		sensitive << x1 << k << m;
+		//dont_initialize();
+	}
+
+	~inverter(){
+	//delete stuff :P
+	}
+
+	void invert(void){				
+		out1=(x1==1 && k==1)?1:0;
+		out2=(x1==0 && m==1)?1:0;
+	}
+};
+
+#endif 
---
+++ 2-to-4-decoder-submodules-2/monitor.h
@@ -0,0 +1,22 @@
+
+#ifndef MONITOR_H_
+#define MONITOR_H_
+#include<iostream>
+#include<systemc.h>
+
+using namespace std;
+
+SC_MODULE(monitor){
+sc_in<bool> m0, m1, m2, m3, m4, m5, m6;
+
+SC_CTOR(monitor){
+	SC_METHOD(monita);
+	dont_initialize();
+}
+
+
+void monita(void){
+		cout<<"at "<<sc_time_stamp()<<" def_binary_input: "<<m0<<" 1st_binary_input: "<<m1<<" 2nd_binary_input: "<<m2<<" d_out1: "<<m3<<" d_out2 "<<m4<<"  d_out3 "<<m5<<" d_out4 "<<m6<<endl;
+	}
+};
+#endif /* MONITOR_H_ */
