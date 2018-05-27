5ba6f7cad7bb1eec304dac93688445970ad097b6
idachi rufusy <ruffiam@yahoo.com>
2018-05-27 19:19:03 +0300

    Add files via upload

--- 2-to-4-line-decoder-submodules_1/driver.h
+++ 2-to-4-line-decoder-submodules_1/driver.h
@@ -1,1 +1,28 @@
 
+#ifndef DRIVER_H_
+#define DRIVER_H_
+
+#include<systemc>
+
+SC_MODULE(driver){
+	sc_out<bool> d_A0;
+	sc_out<bool> d_A1;
+
+	SC_CTOR(driver){
+		SC_THREAD(drive);
+	}
+
+	void drive(void){
+		while(1){
+			d_A0 = 0; d_A1 = 0;
+			wait(5,SC_NS);
+			d_A0 = 1; d_A1 = 0;
+			wait(5,SC_NS);
+			d_A0 = 0; d_A1 = 1;
+			wait(5,SC_NS);
+			d_A0 = 1; d_A1 = 1;
+			wait(5,SC_NS);
+		}
+	}
+};
+#endif /* DRIVER_H_ */
