0e62792d74768a2134168567535dcb5bef4bb130
lundih <32468250+lundih@users.noreply.github.com>
2018-06-01 16:27:04 +0300

    Implementation decoder_2by4 using inverter
    Add inverter.h file to project
    Implement inverter.h in decoder_1by2.h

--- decoder2by4/driver.h
+++ decoder2by4/driver.h
@@ -11,23 +11,23 @@
 #include<systemc>
 
 SC_MODULE(driver){
-sc_out<bool> d_a,d_b;
+	sc_out<bool> d_a, d_b;
 
-SC_CTOR(driver){
-	SC_THREAD(drive);
-}
-
-void drive(void){
-	while(1){
-		d_a=0; d_b=0;
-		wait(5,SC_NS);
-		d_a=0; d_b=1;
-		wait(5,SC_NS);
-		d_a=1; d_b=0;
-		wait(5,SC_NS);
-		d_a=1; d_b=1;
-		wait(5,SC_NS);
+	SC_CTOR(driver){
+		SC_THREAD(drive);
 	}
-}
+
+	void drive(void){
+		while(1){
+			d_a=0,d_b=0;
+			wait(5,SC_NS);
+			d_a=0,d_b=1;
+			wait(5,SC_NS);
+			d_a=1,d_b=0;
+			wait(5,SC_NS);
+			d_a=1,d_b=1;
+			wait(5,SC_NS);
+		}
+	}
 };
 #endif /* DRIVER_H_ */
--- decoder2by4/monitor.h
+++ decoder2by4/monitor.h
@@ -13,16 +13,16 @@
 using namespace std;
 
 SC_MODULE(monitor){
-sc_in<bool> m_a, m_b, m_c, m_d, m_e, m_f;
+sc_in<bool> m_a, m_b,m_c,m_d,m_e,m_f;
 
 SC_CTOR(monitor){
-		SC_METHOD(monita);
-		sensitive<<m_c<<m_d<<m_e<<m_f;
-		dont_initialize();
+	SC_METHOD(monita);
+	sensitive<<m_b<<m_c<<m_e<<m_f;
+	dont_initialize();
 }
 
 void monita(void){
-cout<<"at "<<sc_time_stamp()<<" inputs are: "<<m_a<<" and "<<m_b<<" outputs are: "<<m_c<<" , "<<m_d<<" , "<<m_e<<" and "<<m_f<<endl;
+cout<<"At "<<sc_time_stamp()<<" inputs are: "<<m_a<<" and "<<m_d<<" outputs are: "<<m_b<<" and "<<m_c<<" and "<<m_e<<" and "<<m_f<<endl;
 }
 };
 #endif /* MONITOR_H_ */
