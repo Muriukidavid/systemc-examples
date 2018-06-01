1e9546c65fd1bf226d91c931c2c2338748a40d3c
kev.dev <kev.dev@gmail.com>
2018-06-01 12:03:21 +0300

    Add and gates to decoder 2by4 implementation

--- decoder2by4/decoder_2by4.h
+++ decoder2by4/decoder_2by4.h
@@ -10,15 +10,24 @@
 #include<systemc.h>
 
 #include "../decoder1by2/decoder_1by2.h"
+#include "and_gate.h"
 
 SC_MODULE(decoder2by4) {
 //input and output ports
 	sc_in<bool> a, b;
-	sc_out<bool> c, d, e, f;
+	sc_signal<bool> c, d, e, f;
+
+	sc_out<bool> out_1, out_2, out_3, out_4;
 
 	// Instances of decoder 1by2
 	decoder *dec_1;
 	decoder *dec_2;
+
+	// Instances of and gates
+	and_gate *gate_1;
+	and_gate *gate_2;
+	and_gate *gate_3;
+	and_gate *gate_4;
 
 //constructor: where the processes are bound to simulation kernel
 	SC_CTOR(decoder2by4) {
@@ -28,6 +37,11 @@ SC_MODULE(decoder2by4) {
 		dec_1 = new decoder("dec_1by2_1");
 		dec_2 = new decoder("dec_1by2_2");
 
+		gate_1 = new and_gate("gate_1");
+		gate_2 = new and_gate("gate_2");
+		gate_3 = new and_gate("gate_3");
+		gate_4 = new and_gate("gate_4");
+
 		dec_1->a(a);
 		dec_2->a(b);
 
@@ -36,6 +50,23 @@ SC_MODULE(decoder2by4) {
 
 		dec_2->b(e);
 		dec_2->c(f);
+
+		gate_1->a(c);
+		gate_1->b(e);
+		gate_1->c(out_1);
+
+		gate_2->a(c);
+		gate_2->b(f);
+		gate_2->c(out_2);
+
+		gate_3->a(d);
+		gate_3->b(e);
+		gate_3->c(out_3);
+
+		gate_4->a(f);
+		gate_4->b(d);
+		gate_4->c(out_4);
+
 	}
 
 	~decoder2by4() {
