6d1a1e8674c9b7e1d9a2b53d1871dd852cdebe57
kev.dev <kev.dev@gmail.com>
2018-06-01 11:39:30 +0300

    Implement decoder 2by4 using instances of decoder 1by2

--- decoder2by4/decoder_2by4.h
+++ decoder2by4/decoder_2by4.h
@@ -9,27 +9,40 @@
 #define DECODER_2BY4_H_
 #include<systemc.h>
 
+#include "../decoder1by2/decoder_1by2.h"
+
 SC_MODULE(decoder2by4) {
 //input and output ports
 	sc_in<bool> a, b;
 	sc_out<bool> c, d, e, f;
 
+	// Instances of decoder 1by2
+	decoder *dec_1;
+	decoder *dec_2;
+
 //constructor: where the processes are bound to simulation kernel
 	SC_CTOR(decoder2by4) {
 		SC_METHOD(decode);
 		sensitive<<a<<b;
-		//dont_initialize();
+
+		dec_1 = new decoder("dec_1by2_1");
+		dec_2 = new decoder("dec_1by2_2");
+
+		dec_1->a(a);
+		dec_2->a(b);
+
+		dec_1->b(c);
+		dec_1->c(d);
+
+		dec_2->b(e);
+		dec_2->c(f);
 	}
 
 	~decoder2by4() {
-//delete stuff :P
 	}
 
 	void decode(void) {
-		c = ( b==0 && a==0 )?1:0;
-		d = ( b==0 && a==1 )?1:0;
-		e = ( b==1 && a==0 )?1:0;
-		f = ( b==1 && a==1 )?1:0;
+
 	}
 };
 
