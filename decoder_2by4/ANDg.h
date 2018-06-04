1bfd4963733a6e8e8ee8d4da2ca0683b249b27af
bowen17 <stevenbowyen17@gmail.com>
2018-06-04 12:33:51 +0300

    two by four decoder implementation

---
+++ decoder_2by4/ANDg.h
@@ -0,0 +1,28 @@
+#ifndef ANDg_OPERATION_H_
+#define ANDg_OPERATION_H_
+#include<iostream>
+#include<systemc>
+
+SC_MODULE(anding_gate){
+	// input and output ports
+	sc_in<bool> g1_a, g1_b, g2_a, g2_b, g3_a, g3_b, g4_a, g4_b;
+	sc_out<bool> g1_c, g2_c, g3_c, g4_c;
+
+	SC_CTOR(anding_gate){
+		SC_METHOD(anding);
+		sensitive<<g1_a<<g1_b<<g2_a<<g2_b<<g3_a<<g3_b<<g4_a<<g4_b;	
+	}
+
+	~anding_gate(){}
+
+	void anding(void){
+			g1_c=(g1_a==1 && g1_b==1)?1:0;
+			g2_c=(g2_a==1 && g2_b==1)?1:0;
+			g3_c=(g3_a==1 && g3_b==1)?1:0;
+			g4_c=(g4_a==1 && g4_b==1)?1:0;
+	}
+};
+
+#endif
+
+
---
+++ decoder_2by4/decoder_2by4.h
@@ -0,0 +1,29 @@
+#ifndef DECODER_2BY4_H_
+#define DECODER_2BY4_H_
+#include<systemc.h>
+
+SC_MODULE(decoder){
+	//input and output ports
+	sc_in<bool> in;
+	sc_out<bool> out1,out2;
+	//constructor: where the processes are bound to simulation kernel
+	SC_CTOR(decoder){
+		SC_METHOD(decode);
+		sensitive<<in;
+		//dont_initialize();
+	}
+
+	~decoder(){
+	//delete stuff :P
+	}
+
+	void decode(void){
+		out1=(in==0)?1:0;
+		out2=(in==1)?1:0;
+	}
+};
+
+
+
+
+#endif /* DECODER_2BY4_H_ */
