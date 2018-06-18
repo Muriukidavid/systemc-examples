8a936dee668140980dd5f99a681d9bc21accc54d
idachi rufusy <ruffiam@yahoo.com>
2018-06-18 23:44:33 +0300

    D-flipflop with details
    These files have been edited to be used in the implementation of the DFF with more details. A README and makefile have been added to the lab.

--- D-flipflop/dff.cc
+++ D-flipflop/dff.cc
@@ -1,7 +1,12 @@
 #include "dff.h"
 
 void dff:: dff_method (void){
-	dout=din;
+	
+	dout1 = (din1==1)?0:1;
+	dout2 = (din2==1)?0:1;
+	dout3 = (din7==0)?1:0;
+	dout4 = (din4==1 && din3==1)?1:0;
+	dout5 = (din5==1 && din6==1)?0:1; 
 }
 
 dff:: ~dff(){
--- D-flipflop/dff.h
+++ D-flipflop/dff.h
@@ -1,9 +1,20 @@
 #include <systemc.h>
 
 SC_MODULE (dff) {
-	sc_in <bool> clk;
-	sc_in <bool> din;
-	sc_out <bool> dout;
+	sc_in<bool> clk;
+	sc_in<bool> din1;
+	sc_in<bool> din2;
+	sc_in<bool> din3;
+	sc_in<bool> din4;
+	sc_in<bool> din5;
+	sc_in<bool> din6;
+	sc_in<bool> din7;
+
+	sc_out<bool> dout1;
+	sc_out<bool> dout2;
+	sc_out<bool> dout3;
+	sc_out<bool> dout4;
+	sc_out<bool> dout5;
 
 	void dff_method(void);
 	~dff();
