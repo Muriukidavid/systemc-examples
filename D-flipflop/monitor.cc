b73967a0762aaab2134df00a3e297ac72e32d8dd
rufusy idachi <ruffiam@yahoo.com>
2018-06-19 10:44:17 +0300

    directory had problems

--- D-flipflop/monitor.cc
+++
@@ -1,9 +0,0 @@
-#include "monitor.h"
-#include<iostream>
-
-using namespace std;
-
-void monitor::mon(void){
-	//cout<<"at "<< sc_timestamp() <<" input is: "<<m_din<<" output is: "<<m_dout<<endl;
-	cout<<"input_is: "<<m_din<<" output_is: "<<m_dout<<endl;
-}
