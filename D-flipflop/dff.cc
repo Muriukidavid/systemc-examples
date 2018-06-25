473b790ffcde9c44dec58ba64eca17f912029fbf
Evanshango <lutatwaevans@gmail.com>
2018-06-25 22:29:20 +0300

    Remove D-flipflop directory
    delete initial D-flipflop folder

--- D-flipflop/dff.cc
+++
@@ -1,9 +0,0 @@
-#include "dff.h"
-
-void dff:: dff_method (void){
-	sig_in=din;
-}
-
-dff:: ~dff(){
-	//delete gt1;
-}
--- D-flipflop/nandgate.cc
+++
@@ -1,9 +0,0 @@
-#include "nandgate.h"
-
-void nandgate:: nandgate_method (void){
-	out3 = (!(in1 && in2));
-}
-
-nandgate:: ~nandgate(){
-	//delete gate;
-}
