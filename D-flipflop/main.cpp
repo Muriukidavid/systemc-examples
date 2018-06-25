c09eb36a95f3e37bdcc374d8a4c09f9ffbab5785
SteveOtienoh <stevevoste19@gmail.com>
2018-06-26 00:06:31 +0300

    Add Details to the D-flipflop
    Add and implement inverter.h
    Add and implement nandgate.h

--- D-flipflop/main.cpp
+++ D-flipflop/main.cpp
@@ -24,6 +24,11 @@ int sc_main(int argc, char* argv[])
 	sc_trace_file *fp;
 	fp=sc_create_vcd_trace_file("vcd_trace");
 	fp->set_time_unit(1, SC_NS);
+
+	sc_trace(fp, s_din, "Input(D)");
+	sc_trace(fp, clock, "CLK");
+	sc_trace(fp, s_dout, "Output(Q)");
+
 	sc_start(20, SC_NS);
 	sc_close_vcd_trace_file(fp);
 	return 0;
