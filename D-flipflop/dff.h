0956b8fa3d6105947dc837b57f04dbf2a54126a2
Evanshango <lutatwaevans@gmail.com>
2018-06-25 22:14:10 +0300

    Add D-flipflop directory
    add initial files for D-flipflop

---
+++ D-flipflop/dff.h
@@ -0,0 +1,15 @@
+#include <systemc.h>
+
+SC_MODULE (dff) {
+	sc_in <bool> clk;
+	sc_in <bool> din;
+	sc_out <bool> dout;
+
+	void dff_method(void);
+	~dff();
+
+	SC_CTOR (dff) {
+		SC_METHOD (dff_method);
+			sensitive_pos << clk;
+	}
+};
