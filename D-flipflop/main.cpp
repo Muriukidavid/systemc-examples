45e0808e73cd1f2a7edf8630ba3593d9a51104ae
Evanshango <lutatwaevans@gmail.com>
2018-06-26 02:42:12 +0300

    Add Makefile for D-flipflop
    modify dff.h to remove errors
    change time units in driver.cc and main.cpp for output signals
    correct errors in monitor.cc

--- D-flipflop/main.cpp
+++ D-flipflop/main.cpp
@@ -5,8 +5,8 @@
 
 int sc_main(int argc, char* argv[])
 {
-	sc_signal<bool> s_din, s_dout;
-	sc_clock clock("clk",5,SC_NS,0.5);
+	sc_signal<bool> s_din, s_dout, signal1;
+	sc_clock clock("clk",5,SC_PS,0.5);
 
 	dff dff1("dff");
 	driver dr("driver");
@@ -23,8 +23,13 @@ int sc_main(int argc, char* argv[])
 	
 	sc_trace_file *fp;
 	fp=sc_create_vcd_trace_file("vcd_trace");
-	fp->set_time_unit(1, SC_NS);
-	sc_start(20, SC_NS);
+
+	sc_trace(fp, s_din, "Input(D)");
+	sc_trace(fp, clock, "CLK");
+	sc_trace(fp, signal1, "Output(Q1)");
+
+	fp->set_time_unit(1, SC_PS);
+	sc_start(20, SC_PS);
 	sc_close_vcd_trace_file(fp);
 	return 0;
 }
