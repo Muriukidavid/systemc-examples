ccb4981ef74189e9f06a2920bfc1fa2fdc7fc409
Sarahanyan <saranyango@gmail.com>
2018-07-02 23:17:00 +0300

    Cleaning and organizing the master branch

--- D-flipflop/monitor.cc
+++
@@ -1,8 +0,0 @@
-#include "monitor.h"
-#include<iostream>
-
-using namespace std;
-
-void monitor::mon(void){
-	cout<<"at "<<sc_time_stamp()<<" input is: "<<m_din<<" output is: "<<m_dff1dff2<< "  " <<m_dff2dff3<< "  " <<m_dff3dff4<< "  " <<m_dout<<endl;
-}
--- D-flipflop/nandgate.cc
+++
@@ -1,9 +0,0 @@
-#include "nandgate.h"
-
-void nandgate:: nandgate_method (void){
-	z_nand = (!(x_nand&&y_nand));
-}
-
-nandgate:: ~nandgate(){
-	//delete gt1;
-}
