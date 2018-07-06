01d13deb3472b2d9f66b0707597ec86362e3ad40
brianshiva <39331667+brianshiva@users.noreply.github.com>
2018-07-06 13:02:49 +0300

    003

---
+++ decoder2by4/monitor.h
@@ -0,0 +1,28 @@
+/*
+ * decoder_2by4.cc
+ *
+ *  Created on: May 27, 2018
+ *      Author: roberto
+ */
+
+#ifndef MONITOR_H_
+#define MONITOR_H_
+#include<iostream>
+#include<systemc>
+
+using namespace std;
+
+SC_MODULE(monitor){
+sc_in<bool> m_a, m_b, m_c, m_d, m_e, m_f;
+
+SC_CTOR(monitor){
+	SC_METHOD(monita);
+	sensitive<<m_a<<m_b<<m_c<<m_d<<m_e<<m_f;
+	dont_initialize();
+}
+
+void monita(void){
+cout<<"at "<<sc_time_stamp()<<" input is: "<<m_f<<" & "<<m_e<<" outputs are: "<<m_a <<"   "<<m_b <<"   "<<m_c <<"   "<<m_d <<endl;
+}
+};
+#endif /* MONITOR_H_ */
